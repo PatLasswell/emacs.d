3f73970f9e69a415cd39564b7b4c745953a2b1c0
imofftoseethewizard <imofftoseethewizard@gmail.com>
2012-12-03 01:25:27 +0100

    font lock colors

--- init.el
+++ init.el
@@ -11,5 +11,9 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
+ '(font-lock-builtin-face ((((class color) (min-colors 88) (background light)) (:foreground "#0066cc"))))
+ '(font-lock-comment-face ((((class color) (min-colors 88) (background light)) (:foreground "#999933"))))
+ '(font-lock-function-name-face ((((class color) (min-colors 88) (background light)) (:foreground "#ffcc66"))))
+ '(font-lock-string-face ((((class color) (min-colors 88) (background light)) (:foreground "green"))))
  '(mode-line ((((class color) (min-colors 88)) (:background "grey30" :foreground "green"))))
  '(mode-line-inactive ((default (:inherit mode-line)) (((class color) (min-colors 88) (background light)) (:background "grey30" :foreground "grey90" :weight light)))))
